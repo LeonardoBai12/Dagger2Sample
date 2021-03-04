c39c881dbeb418e623a1e5f9f3544f6df8195c15
LeonardoBai12 <69223422+LeonardoBai12@users.noreply.github.com>
2021-03-04 13:17:44 -0300

    Added logout button

--- app/src/main/java/com/example/daggersample/main/MainActivity.kt
+++ app/src/main/java/com/example/daggersample/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.daggersample.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.example.daggersample.BaseActivity
 import com.example.daggersample.R
 import dagger.android.support.DaggerAppCompatActivity
@@ -12,6 +15,18 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.logout) {
+            sessionManager.logout()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     companion object{
         const val TAG = "MainActivity"
