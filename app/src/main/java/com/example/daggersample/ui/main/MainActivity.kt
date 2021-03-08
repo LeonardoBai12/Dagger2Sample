b70c6a908836368c312c35e4158ab0383c5d890c
LeonardoBai12 <69223422+LeonardoBai12@users.noreply.github.com>
2021-03-08 17:05:04 -0300

    Adicionado controle dos fragments via XML

--- app/src/main/java/com/example/daggersample/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/daggersample/ui/main/MainActivity.kt
@@ -3,23 +3,35 @@ package com.example.daggersample.ui.main
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.NonNull
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.daggersample.BaseActivity
 import com.example.daggersample.R
-import com.example.daggersample.ui.main.posts.PostFragment
-import com.example.daggersample.ui.main.profile.ProfileFragment
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : BaseActivity() {
+
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        testFragment();
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        init()
     }
 
-    fun testFragment(){
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.main_container, PostFragment())
-                .commit();
+    fun init(){
+        val navgationController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navgationController, drawerLayout)
+        NavigationUI.setupWithNavController(navigationView, navgationController)
+        navigationView.setNavigationItemSelectedListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,12 +43,60 @@ class MainActivity : BaseActivity() {
         if(item.itemId == R.id.logout) {
             sessionManager.logout()
             return true
+        } else if(item.itemId == R.id.home){
+            if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+                drawerLayout.closeDrawer(GravityCompat.START)
+                return true
+            }
+            else{
+                return false
+            }
         }
         return super.onOptionsItemSelected(item)
     }
 
     companion object{
         const val TAG = "MainActivity"
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_profile -> {
+
+                val navOptions = NavOptions.Builder()
+                        .setPopUpTo(R.id.main, true)
+                        .build()
+                Navigation.findNavController(this, R.id.nav_host_fragment)
+                        .navigate(R.id.profileScreen,
+                                null,
+                                navOptions
+                        )
+            }
+            R.id.nav_posts -> {
+                if (isValidDestination(R.id.postsScreen)) {
+                    Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.postsScreen)
+                }
+            }
+        }
+        menuItem.isChecked = true
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    fun isValidDestination(destination: Int): Boolean {
+        return destination != Navigation.findNavController(this, R.id.nav_host_fragment).currentDestination!!.id
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawerLayout)
     }
 
 }
